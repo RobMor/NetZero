562b0d92ae660f330d0ff26e4a2d272078e22537
Robbie Morrison <robbieguy98@gmail.com>
2019-07-09 21:58:19 -0400

    Updated solar data source

--- netzero/sources/solar.py
+++ netzero/sources/solar.py
@@ -1,150 +1,149 @@
-import json, requests, datetime, sqlite3, progressbar
+"""Data source specification for Solar Edge data.
+
+Solar Edge API documentation (ca 2019):
+https://www.solaredge.com/sites/default/files/se_monitoring_api.pdf
+"""
+
+import json
+import requests
+import datetime
+import sqlite3
+
+from netzero.sources.base import DataSource
 
 
-def get_data(conn, site, api_key, start_date=datetime.datetime(2016, 1, 27), end_date=datetime.datetime.today()):
-    """
-    Orchestrates the collection of solar data from Solar Edge
+class Solar(DataSource):
+    default_start = datetime.datetime(2016, 1, 27)
+    default_end = datetime.datetime.today()
+    def __init__(self, config, conn):
+        super().__init__(config, entry="solar", fields=["api_key", "site_id"])
 
-    :param conn: An sqlite database connection
-    :param site: The siteID to retrieve data from
-    :param api_key: The api key to access Solar Edges API
-    :param start: The start of the time interval to collect data from
-    :param end: The end of the time interval to collect data from
-    """
-    cur = conn.cursor()
+        self.api_key = config["solar"]["api_key"]
+        self.site_id = config["solar"]["site_id"]
 
-    # Make sure that the solar_raw table is already in the database
-    cur.execute("""SELECT name FROM sqlite_master 
-    WHERE type='table' and name='solar_raw'""")
-    if not cur.fetchall(): # Table isn't in database
-        cur.execute("""
-        CREATE TABLE solar_raw(
-            time TEXT PRIMARY KEY,
-            value INTEGER
-        )""")
-        conn.commit()
+        self.conn = conn
 
-    # Make sure that the solar_day table is already in the database
-    cur.execute("""SELECT name FROM sqlite_master 
-    WHERE type='table' and name='solar_day'""")
-    if not cur.fetchall(): # Table isn't in database
-        cur.execute("""
-        CREATE TABLE solar_day(
-            day TEXT PRIMARY KEY,
-            value INTEGER NOT NULL
-        )""")
-        conn.commit()
+        cursor = self.conn.cursor()
 
-    # Collect the raw data
-    store_raw_data(conn, site, api_key, start_date, end_date)
+        # Create the table for the raw data
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS solar_raw(time TEXT PRIMARY KEY, value REAL)
+        """)
+        
+        # Create the table for the processed data
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS solar_day(date TEXT PRIMARY KEY, value REAL)
+        """)
 
-    # Calculate daily usage and store in a database
-    calculate_daily(conn)
+        self.conn.commit()
 
+    def collect_data(self, start_date=None, end_date=None):
+        """Collect raw solar data from SolarEdge
 
-def query_api(key, site, span):
-    """
-    A method to query the Solar Edge api for energy data
+        Collects data using the SolarEdge API, storing it in the database.
+        
+        Parameters
+        ----------
+        start : datetime.datetime
+            The end of the time interval to collect data from
+        end : datetime.datetime
+            The end of the time interval to collect data from
+        """
+        if start_date is None:
+            start_date = self.default_start
+        if end_date is None:
+            end_date = self.default_end
 
-    :param key: The api_key to be fed to the API, given to the user by SolarEdge
-    :param site: The Site ID to collect the data from
-    :param span: The span of time to query the API for (must be less than a month)
-    :returns: The API response in python dict format:
-        {
-            "energy":{
-                "timeUnit": _,
-                "unit":_,
-                "values":[
-                    {
-                        "date":"YYYY-MM-DD HH:MM:SS",
-                        "value":_
-                    }, ...
-                ]
+        cursor = self.conn.cursor()
+    
+        # Create a range of month long intervals
+        intervals = self.create_monthly_intervals(start_date, end_date)
+
+        # Iterate through each date range
+        for interval in intervals:
+            result = self.query_api(interval)
+
+            for entry in result["energy"]["values"]:
+                # Parse the time from the given string
+                date = entry["date"]
+                value = entry["value"] or 0  # 0 if None
+
+                cursor.execute("""
+                    INSERT OR IGNORE INTO solar_raw(time, value) VALUES(?,?)
+                """, (date, value))
+
+                print("SOLAR:", date, "--", value)
+        
+        self.conn.commit()
+
+    def create_monthly_intervals(self, start_date: datetime.datetime, end_date: datetime.datetime):
+        """
+        Computes a list of tuples (datetime, datetime) that represent month long
+        intervals of time to most compactly collect all the solar data we possibly
+        can. The SolarEdge API only offers quarter of an hour granularity on a
+        monthly interval limit.
+        """
+        month = datetime.timedelta(days=30)
+
+        intervals = []
+        prev = start_date
+        while (prev + month) < end_date:
+            intervals.append((prev, prev+month))
+            prev = prev+month
+
+        intervals.append((prev, end_date))
+
+        return intervals
+
+    def query_api(self, interval):
+        """A method to query the Solar Edge api for energy data
+
+        Parameters
+        ----------
+        span : Tuple[datetime.datetime, datetime.datetime]
+            The span of time to query the API for (must be less than a month)
+
+        Returns
+        -------
+        The API response in python dict format:
+            {
+                "energy":{
+                    "timeUnit": _,
+                    "unit":_,
+                    "values":[
+                        {
+                            "date":"YYYY-MM-DD HH:MM:SS",
+                            "value":_
+                        }, ...
+                    ]
+                }
             }
+        """
+        payload = {
+            "api_key": self.api_key,
+            "startDate": interval[0].strftime("%Y-%m-%d"),
+            "endDate": interval[1].strftime("%Y-%m-%d"),
+            # Even though we condense this data down to a daily sum we still want to
+            # collect as much data as possible because perhaps it may some day be
+            # useful. Because of this we do quarter of an hour
+            "timeUnit": "QUARTER_OF_AN_HOUR"
         }
-    """
-    payload = {
-        "api_key": key,
-        "startDate": span[0].strftime("%Y-%m-%d"),
-        "endDate": span[1].strftime("%Y-%m-%d"),
-        "timeUnit": "QUARTER_OF_AN_HOUR"
-        # Even though we condense this data down to a daily sum we still want to
-        # collect as much data as possible because perhaps it may some day be
-        # useful.
-    }
-    data = requests.get("https://monitoringapi.solaredge.com/site/"+site+"/energy.json", params=payload)
-    return json.loads(data.text)
+        data = requests.get("https://monitoringapi.solaredge.com/site/"+self.site_id+"/energy.json", params=payload)
+        return json.loads(data.text)
 
+    def process_data(self):
+        """Computes the daily input of the solar panels in kWh."""
+        cursor = self.conn.cursor()
 
-def monthly_intervals(start_date, end_date):
-    """
-    Computes a list of tuples (datetime, datetime) that represent month long
-    intervals of time to most compactly collect all the solar data we possibly
-    can. The SolarEdge API only offers quarter of an hour granularity on a
-    monthly interval limit.
-    """
-    month = datetime.timedelta(days=30)
+        # Developers note, these dates are in EST already so we can just directly
+        # convert them
+        cursor.execute("""
+        INSERT OR IGNORE INTO solar_day
+        SELECT
+            DATE(time, 'start of day') AS day,
+            SUM(value) / 1000.0
+        FROM solar_raw GROUP BY day
+        """)
 
-    intervals = []
-    prev = start_date
-    while (prev + month) < end_date:
-        intervals.append((prev, prev+month))
-        prev = prev+month
+        self.conn.commit()
 
-    intervals.append((prev, end_date))
-
-    return intervals
-
-
-widgets = ["Solar: Downloading ", progressbar.SimpleProgress(), progressbar.Bar(), " ", progressbar.ETA()]
-
-def store_raw_data(conn, site, api_key, start_date, end_date):
-    """
-    Collects and stores the raw data from the SolarEdge API in our database.
-
-    :param conn: An sqlite database connection
-    :param site: The SolarEdge site id to collect data for
-    :param api_key: The SolarEdge API key provided by them
-    :param start_date: The start of the time interval to collect data on
-    :
-    """
-    cur = conn.cursor()
-    
-    # Create a range of month long intervals
-    spans = monthly_intervals(start_date, end_date)
-    
-    # Create a visual progress bar.
-    bar = progressbar.progressbar(spans , widgets=widgets, redirect_stdout=True)
-
-    # Iterate through each date range
-    for span in bar:
-        result = query_api(api_key, site, span)
-
-        for entry in result["energy"]["values"]:
-            # Parse the time from the given string
-            date = entry["date"] #Arrow.strptime(entry["date"], "%Y-%m-%d %X", tzinfo="EST").timestamp
-            value = entry["value"] or 0  # 0 if None
-
-            cur.execute("""INSERT OR IGNORE INTO solar_raw(time, value)
-                VALUES(?,?)""", (date, value))
-    
-    conn.commit()
-
-
-def calculate_daily(conn):
-    """
-    Computes the daily input of the solar panels in kWh.
-    """
-    cur = conn.cursor()
-
-    # Developers note, these dates are in EST already so we can just directly
-    # convert them
-    cur.execute("""
-    INSERT OR IGNORE INTO solar_day 
-    SELECT 
-        DATE(time, 'start of day') AS day,
-        SUM(value) / 1000.0 
-    FROM solar_raw GROUP BY day
-    """)
-
-    conn.commit()
