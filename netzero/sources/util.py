099393495fa1364c69840a537f25a21266b6b6ee
Robbie Morrison <robbieguy98@gmail.com>
2019-07-12 19:23:10 -0400

    Added tests for utils

--- netzero/sources/util.py
+++ netzero/sources/util.py
@@ -1,13 +1,29 @@
 import datetime
 
 
-def time_intervals(start_date, end_date, days=1, seconds=0, microseconds=0, milliseconds=0, minutes=0, hours=0, weeks=0):
-    delta = datetime.timedelta(days=days, seconds=seconds, microseconds=microseconds, milliseconds=milliseconds, minutes=minutes, hours=hours, weeks=weeks)
+def time_intervals(start_date,
+                   end_date,
+                   days=1,
+                   seconds=0,
+                   microseconds=0,
+                   milliseconds=0,
+                   minutes=0,
+                   hours=0,
+                   weeks=0):
+    """A generator that produces inclusive time intervals from start to end date"""
+    if start_date <= end_date:
+        delta = datetime.timedelta(days=days,
+                                   seconds=seconds,
+                                   microseconds=microseconds,
+                                   milliseconds=milliseconds,
+                                   minutes=minutes,
+                                   hours=hours,
+                                   weeks=weeks)
 
-    intervals = []
-    prev = start_date
-    while (prev + delta) < end_date:
-        yield (prev, prev+delta)
-        prev = prev+delta
+        intervals = []
+        prev = start_date
+        while (prev + delta) < end_date:
+            yield (prev, prev + delta)
+            prev = prev + delta
 
-    yield (prev, end_date)
+        yield (prev, end_date)
