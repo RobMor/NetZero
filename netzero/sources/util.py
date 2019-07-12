a154b7fc2e14298144fb81906cd7411304f2d897
Robbie Morrison <robbieguy98@gmail.com>
2019-07-12 18:44:13 -0400

    Updated time_intervals to provide default argument... TODO UGLY

--- netzero/sources/util.py
+++ netzero/sources/util.py
@@ -1,8 +1,8 @@
 import datetime
 
 
-def time_intervals(start_date, end_date, **kwargs):
-    delta = datetime.timedelta(**kwargs)
+def time_intervals(start_date, end_date, days=1, seconds=0, microseconds=0, milliseconds=0, minutes=0, hours=0, weeks=0):
+    delta = datetime.timedelta(days=days, seconds=seconds, microseconds=microseconds, milliseconds=milliseconds, minutes=minutes, hours=hours, weeks=weeks)
 
     intervals = []
     prev = start_date
