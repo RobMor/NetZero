fa678b74a3d13ddbe0868ebde720c6b5b80c668d
Robbie Morrison <robbieguy98@gmail.com>
2019-07-12 18:42:10 -0400

    Updated setup.py

--- setup.py
+++ setup.py
@@ -5,4 +5,4 @@ setup(name='NetZeroData',
       description='A collection of tools to mine data on the efficiency of a house',
       author='Robert Morrison',
       author_email='robbieguy98@gmail.com',
-      install_requires=["requests"])
+      install_requires=["requests", "bs4"])
