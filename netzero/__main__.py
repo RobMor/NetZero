f68886ddfe164cb5b498701ab11f37f9256d6b7a
Robbie Morrison <robbieguy98@gmail.com>
2019-07-13 00:11:30 -0400

    Overhauled command structure

--- netzero/__main__.py
+++ netzero/__main__.py
@@ -13,7 +13,8 @@ def collect_data(arguments):
     else:
         config = None
 
-    conn = sqlite3.connect(arguments.database, detect_types=sqlite3.PARSE_DECLTYPES)
+    conn = sqlite3.connect(arguments.database,
+                           detect_types=sqlite3.PARSE_DECLTYPES)
 
     initialized_sources = []
     for source in arguments.sources:
@@ -21,7 +22,7 @@ def collect_data(arguments):
 
     for source in initialized_sources:
         # Collect the raw data from the source
-        source.collect_data(start_date=arguments.begin, end_date=arguments.end)
+        source.collect_data(start_date=arguments.start, end_date=arguments.end)
 
         # Process the data from the source
         source.process_data()
@@ -35,30 +36,81 @@ def format_data(arguments):
 if __name__ == "__main__":
     import argparse
 
-    parser = argparse.ArgumentParser(description="Automatically collects and formats timeseries data from multiple sources")
+    program_name = "netzero"
+
+    parser = argparse.ArgumentParser(
+        program_name,
+        description="Collects and formats data from multiple sources")
     parser.set_defaults(func=None)
 
-    subparsers = parser.add_subparsers(dest="action")
-    
+    subparsers = parser.add_subparsers(title="available commands",
+                                       metavar="<command>",
+                                       prog=program_name)
+
     # --- Data Collection Arguments ---
-    collect_parser = subparsers.add_parser("collect", description="Collect data")
+    collect_parser = subparsers.add_parser(
+        "collect",
+        description=
+        "Collect data from various sources and store it in a local database",
+        help="Collect data",
+        prefix_chars="-+")
     collect_parser.set_defaults(func=collect_data)
 
-    sources_group = collect_parser.add_argument_group("Data Sources", description="Flags used to select the supported data sources")
-    sources_group.add_argument("-s", "--solar", help="Collects solar data", dest="sources", action="append_const", const=netzero.sources.Solar)
-    sources_group.add_argument("-p", "--pepco", help="Collects pepco data", dest="sources", action="append_const", const=netzero.sources.Pepco)
-    sources_group.add_argument("-g", "--gshp", help="Collects ground source heat pump data", dest="sources", action="append_const", const=netzero.sources.Gshp)
-    sources_group.add_argument("-w", "--weather", help="Collects weather data", dest="sources", action="append_const", const=netzero.sources.Weather)
+    sources_group = collect_parser.add_argument_group(
+        "data sources",
+        description="Flags used to select the supported data sources")
+    sources_group.add_argument("+s",
+                               "--solar",
+                               help="collects solar data",
+                               dest="sources",
+                               action="append_const",
+                               const=netzero.sources.Solar)
+    sources_group.add_argument("+p",
+                               "--pepco",
+                               help="collects pepco data",
+                               dest="sources",
+                               action="append_const",
+                               const=netzero.sources.Pepco)
+    sources_group.add_argument("+g",
+                               "--gshp",
+                               help="collects ground source heat pump data",
+                               dest="sources",
+                               action="append_const",
+                               const=netzero.sources.Gshp)
+    sources_group.add_argument("+w",
+                               "--weather",
+                               help="collects weather data",
+                               dest="sources",
+                               action="append_const",
+                               const=netzero.sources.Weather)
 
-    collect_parser.add_argument("-c", required=True, metavar="config", help="Loads inputs from the specified JSON file", dest="config", type=argparse.FileType('r'))
+    collect_parser.add_argument("-s",
+                                "--start",
+                                metavar="YYYY-MM-DD",
+                                help="start date for date range",
+                                dest="start",
+                                type=datetime.date.fromisoformat)
+    collect_parser.add_argument("-e",
+                                "--end",
+                                metavar="YYYY-MM-DD",
+                                help="end date for date range",
+                                dest="end",
+                                type=datetime.date.fromisoformat)
 
-    collect_parser.add_argument("-b", "--begin", metavar="YYYY-MM-DD", help="Start date for date range", dest="begin", type=datetime.date.fromisoformat)
-    collect_parser.add_argument("-e", "--end", metavar="YYYY-MM-DD", help="End date for date range", dest="end", type=datetime.date.fromisoformat)
+    collect_parser.add_argument(
+        "-c",
+        required=True,
+        metavar="config",
+        help="loads inputs from the specified JSON file",
+        dest="config",
+        type=argparse.FileType('r'))
 
     collect_parser.add_argument("database")
 
     # --- Formatting Arguments ---
-    format_parser = subparsers.add_parser("format", description="Format data")
+    format_parser = subparsers.add_parser("format",
+                                          description="Format data",
+                                          help="Format data")
     format_parser.set_defaults(func=format_data)
 
     # --- Logic ---
