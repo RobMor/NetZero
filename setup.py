006713eee2addcfdad42fe1f03b01f39db55a6a1
Robert Morrison <robbieguy98@gmail.com>
2019-08-03 20:55:43 -0400

    Fix setup to depend on pandas

--- setup.py
+++ setup.py
@@ -5,23 +5,16 @@ from setuptools import find_packages, setup
 
 dependencies = ["requests", "bs4"]
 
-setup(name="netzero",
-      version="0.1.0",
-      author="Robert Morrison",
-      author_email="robbieguy98@gmail.com",
-      description=
-      "A collection of tools to mine data on the efficiency of a house",
-      long_description=__doc__,
-
-      url="https://github.com/RobMor/NetZero",
-
-      packages=["netzero", "netzero.sources"],
-
-      install_requires=["requests", "bs4"],
-
-      entry_points = {
-            "console_scripts": [
-                  "netzero=netzero.__main__:main"
-            ]
-      },
+setup(
+    name="netzero",
+    version="0.1.0",
+    author="Robert Morrison",
+    author_email="robbieguy98@gmail.com",
+    description=
+    "A collection of tools to mine data on the efficiency of a house",
+    long_description=__doc__,
+    url="https://github.com/RobMor/NetZero",
+    packages=["netzero", "netzero.sources"],
+    install_requires=["requests", "bs4", "pandas"],
+    entry_points={"console_scripts": ["netzero=netzero.__main__:main"]},
 )
