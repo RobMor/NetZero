8d26184105644f65d720b5ba80774655a635a9da
Robert Morrison <robbieguy98@gmail.com>
2020-01-14 13:22:04 -0500

    Removed old data source code

--- netzero/sources.py
+++ netzero/sources.py
@@ -1,37 +1,5 @@
-import abc
-
 import entrypoints
 
-
-class DataSource(abc.ABC):
-    TIME = "time"
-
-    @property
-    @abc.abstractmethod
-    def summary(self):
-        """Returns a string containing a one line summary of this data source."""
-        pass
-
-    @abc.abstractmethod
-    def __init__(self, config):
-        """Sets up the data source's internal state based on the config."""
-        pass
-
-    @property
-    @abc.abstractmethod
-    def columns(self):
-        """An iterable containing column names to add to table"""
-        pass
-
-    @abc.abstractmethod
-    def collect_data(self, start=None, end=None):
-        """Collects the data between the start and end date (inclusive)"""
-        pass
-
-    @abc.abstractmethod
-    def aggregators(self):
-        """Returns an dict from column tuples to sqlite3 aggregators"""
-        pass
 
 def load():
     for name, entrypoint in entrypoints.get_group_named("netzero.sources").items():
@@ -39,7 +7,7 @@ def load():
         yield name, source
 
 
-def add_source_args(parser):
+def add_args(parser):
     sources_group = parser.add_argument_group(
         "data sources",
         description="Flags used to select the supported data sources")
@@ -47,7 +15,6 @@ def add_source_args(parser):
     for name, source in load():
         if not hasattr(source, "name"):
             source.name = name
-        # TODO potential short hand collisions
         if not hasattr(source, "option"):
             source.option = source.name[0]
         if not hasattr(source, "long_option"):
